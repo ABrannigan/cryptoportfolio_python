03d415da7c6e3b885e0dbec094d54c41f5c56d7e
adam <ab-139@hotmail.com>
2018-10-15 21:54:10 +0100

    initial commit

---
+++ cryptoportfolio.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 15 17:48:25 2018
+
+@author: adam
+"""
+
+def updatefolio():
+    while True:
+        currency = input('Please Enter currency IE: btc')
+        file = open('portfolio.csv', 'w')
+        
+    
+
+
+choice = input('Enter "c" create a new portfolio |Enter "e" to edit and existing portfolio' )
+if choice  == 'c'
+   file = open('portfolio.csv', 'x')
+       
+elif choice =='e'
+       
