0e092ba6949c18065853192e66b78ffc0a2b8f5d
adam <ab-139@hotmail.com>
2018-10-29 10:04:21 +0000

    view protfoilio function added with new choice option not displayed in message yet

--- cryptoportfolio.py
+++ cryptoportfolio.py
@@ -9,6 +9,11 @@ import os
 #import Pyqt5 
 import pandas as pd
 import matplotlib.pyplot as plt
+import json
+import requests
+from datetime import datetime
+from prettytable import PrettyTable
+from colorama import Fore, Back, Style
 
 #pyuic path script coverts ui to py  C:\ProgramData\Anaconda3\Scripts
 # command to run pyuic5 -x "C:\Users\adam\Desktop\test.ui" -o "C:\Users\adam\Desktop\test.py"
@@ -56,13 +61,74 @@ def delcrypto():
                break
             else:
                file =file[file.Currency.str.contains(delcurrency) == False]
-               print(delcurrency + 'has been removed from portfilio')
+               print(delcurrency + ' has been removed from portfilio')
                file.to_csv('portfolio.csv', index = False)
                print(pd.read_csv('portfolio.csv'))
-                
+               
+               
+def viewfolio():
+        '''this function accesses coinmarket cap api 
+            and displays current market info related to user portfolio'''
+           
+        listingsURI = 'https://api.coinmarketcap.com/v2/listings/?convert=USD'
+        
+        api = {
             
+            'X-CMC_PRO_API_KEY': 'c1aa9072-eedf-48a3-8e7d-f5fe74a457b8'
+            
+        }
+        
+        request = requests.get(listingsURI , headers=api)
+        result = request.json() 
+        #data = pd.DataFrame(result['data'])
+        data = result['data']
+       
+        ticker_url_pairs = {}
+        for x in data:
+            symbol = x['symbol']
+            url = x['id']
+            ticker_url_pairs[symbol] = url
+    
+        
+        file = pd.read_csv('portfolio.csv' ) #skiprows=1
+        #print(file)
+        file.columns = map(str.upper, file.columns)
+        
+        total_value = 0.00
+        #price = 0.00
+        table = PrettyTable(['Crypto', 'Amount Owned', 'USD' + ' Value', 'Price'])
+   
+        for i, row in file.iterrows():
+            ticker = row['CURRENCY'] 
+            shares = row['SHARES']
+        
+    
+            ticker_url = 'https://api.coinmarketcap.com/v2/ticker/' + str(ticker_url_pairs[ticker]) + '/' + '?structure=array&convert=' + 'USD'
+            request = requests.get(ticker_url, headers=api)
+            result = request.json()
+            #print(json.dumps(result, sort_keys = True, indent=4))
+            
+            key = result['data'][0]
+            #rank = currency['rank']
+            name = key['name']
+            #last_updated = currency['last_updated']
+            symbol = key['symbol']
+            quotes = key['quotes']['USD']
+            price = quotes['price']
 
-
+            value = round(price * float(shares),2)
+            total_value = round(value + total_value,2)
+                    
+            table.add_row([name + ' (' + symbol + ')', str(shares),'$' +str(value),'$' + str(price)])
+                
+        print(table)
+        print()
+        print('Total Portfolio Value: ' +'$' + str(total_value))
+        print()
+        
+   
+        
+       
 
 while True:
     
@@ -71,7 +137,11 @@ while True:
     
     if choice  == 'r':
        delcrypto()
-       
+    
+    elif choice == 'v':   
+        viewfolio()
+        
+        
     elif choice == 'a':
         addcrypto()
     
